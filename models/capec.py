071b72e5571f6becb149e33284a532ba49bf70c3
velac01 <cody.vela@maccyberdesigns.com>
2023-08-24 18:35:50 -0500

    added fields to capec table

--- models/capec.py
+++ models/capec.py
@@ -21,15 +21,27 @@ class Capec(Base):
     name: Mapped[str] = mapped_column(String(255))
     status: Mapped[str] = mapped_column(String(255))
     description: Mapped[str] = mapped_column(String(4000))
+    likelihood_of_attack: Mapped[str] = mapped_column(String(50))
+    typical_severity: Mapped[str] = mapped_column(String(50))
 
-    def __init__(self, capec_id, name, status, description) -> None:
+    def __init__(
+        self,
+        capec_id,
+        name,
+        status,
+        description,
+        likelihood_of_attack,
+        typical_severity,
+    ) -> None:
         self.capec_id = capec_id
         self.name = name
         self.status = status
         self.description = description
+        self.likelihood_of_attack = likelihood_of_attack
+        self.typical_severity = typical_severity
 
     def __repr__(self):
-        return f"(ID: {self.capec_id}) Name: {self.name} | Status: {self.status} | Desc: {self.description}\n\n"
+        return f"(CAPEC ID: {self.capec_id}) Name: {self.name} \n\n"
 
 
 def parse_capec_csv(file):
@@ -43,7 +55,15 @@ def parse_capec_csv(file):
 def prepop_capec_data(session):
     raw_capec_df = pd.read_csv(data, index_col=False)
     clean_capec_df = raw_capec_df[
-        ["ID", "Name", "Status", "Description", "Related Weaknesses"]
+        [
+            "ID",
+            "Name",
+            "Status",
+            "Description",
+            "Related Weaknesses",
+            "Likelihood Of Attack",
+            "Typical Severity",
+        ]
     ].fillna("N/A")
     capecs = []
     for index, row in tqdm(
@@ -56,7 +76,16 @@ def prepop_capec_data(session):
         name = row["Name"]
         status = row["Status"]
         description = row["Description"]
-        curr = Capec(capec_id=id, name=name, status=status, description=description)
+        likelihood_of_attack = row["Likelihood Of Attack"]
+        typical_severity = row["Typical Severity"]
+        curr = Capec(
+            capec_id=id,
+            name=name,
+            status=status,
+            description=description,
+            likelihood_of_attack=likelihood_of_attack,
+            typical_severity=typical_severity,
+        )
         capecs.append(curr)
     for capec in capecs:
         session.add(capec)
